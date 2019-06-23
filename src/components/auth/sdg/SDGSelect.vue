b1a0d71f2b067f9f2d8a0606fa9aefe0b67068d8
Anthony <agdoueihi@gmail.com>
2019-06-23 11:20:17 +1000

    Functioning SDG Submit Button

--- src/components/auth/sdg/SDGSelect.vue
+++ src/components/auth/sdg/SDGSelect.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="sdg-container">
+  <div id="sdg-container">
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg1"/>
+        <input type="checkbox" id="sdg1" v-model="checkedSDG" value="1"/>
         <label class="sdglabel" for="sdg1">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-01.png"    alt="No Poverty" />
         </label>
@@ -14,7 +14,7 @@
 
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg2"/>
+        <input type="checkbox" id="sdg2" v-model="checkedSDG" value="2" />
         <label class="sdglabel" for="sdg2">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-02.png"    alt="Zero Hunger" />
         </label>
@@ -25,7 +25,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg3"/>
+        <input type="checkbox" id="sdg3" v-model="checkedSDG" value="3" />
         <label class="sdglabel" for="sdg3">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-03.png"    alt="Good Health And Well-Being" />
         </label>
@@ -36,7 +36,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg4"/>
+        <input type="checkbox" id="sdg4" v-model="checkedSDG" value="4" />
         <label class="sdglabel" for="sdg4">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-04.png"    alt="Quality Education" />
         </label>
@@ -47,7 +47,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg5"/>
+        <input type="checkbox" id="sdg5" v-model="checkedSDG" value="5" />
         <label class="sdglabel" for="sdg5">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-05.png"    alt="Gender Equality" />
         </label>
@@ -58,7 +58,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg6"/>
+        <input type="checkbox" id="sdg6" v-model="checkedSDG" value="6" />
         <label class="sdglabel" for="sdg6">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-06.png"    alt="Clean Water and Sanitation" />
         </label>
@@ -69,7 +69,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg7"/>
+        <input type="checkbox" id="sdg7" v-model="checkedSDG" value="7" />
         <label class="sdglabel" for="sdg7">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-07.png"    alt="Affordable Clean Energy" />
         </label>
@@ -80,7 +80,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg8"/>
+        <input type="checkbox" id="sdg8" v-model="checkedSDG" value="8"/>
         <label class="sdglabel" for="sdg8">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-08.png"    alt="Decent Work and Economic Growth" />
         </label>
@@ -91,7 +91,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg9"/>
+        <input type="checkbox" id="sdg9" v-model="checkedSDG" value="9" />
         <label class="sdglabel" for="sdg9">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-09.png"    alt="Industry, Innovation and Infrastructure" />
         </label>
@@ -102,7 +102,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg10"/>
+        <input type="checkbox" id="sdg10" v-model="checkedSDG" value="10"/>
         <label class="sdglabel" for="sdg10">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-10.png"    alt="Reduced Inequality" />
         </label>
@@ -113,7 +113,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg11"/>
+        <input type="checkbox" id="sdg11" v-model="checkedSDG" value="11"/>
         <label class="sdglabel" for="sdg11">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-11.png"    alt="Sustainable Cities and Communities" />
         </label>
@@ -124,7 +124,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg12"/>
+        <input type="checkbox" id="sdg12" v-model="checkedSDG" value="12"/>
         <label class="sdglabel" for="sdg12">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-12.png"    alt="Responsible Consumption and Production" />
         </label>
@@ -135,7 +135,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg13"/>
+        <input type="checkbox" id="sdg13" v-model="checkedSDG" value="13"/>
         <label class="sdglabel" for="sdg13">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-13.png"    alt="Climate Action" />
         </label>
@@ -146,7 +146,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg14"/>
+        <input type="checkbox" id="sdg14" v-model="checkedSDG" value="14"/>
         <label class="sdglabel" for="sdg14">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-14.png"    alt="Life Below Water" />
         </label>
@@ -157,7 +157,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg15"/>
+        <input type="checkbox" id="sdg15" v-model="checkedSDG" value="15"/>
         <label class="sdglabel" for="sdg15">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-15.png"    alt="Life On Land" />
         </label>
@@ -168,7 +168,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg16"/>
+        <input type="checkbox" id="sdg16" v-model="checkedSDG" value="16"/>
         <label class="sdglabel" for="sdg16">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-16.png"    alt="Peace, Justice and Strong Institutions" />
         </label>
@@ -179,7 +179,7 @@
     </div>
     <div class="sdg">
       <div class="sdg-checkbox">
-        <input type="checkbox" id="sdg17"/>
+        <input type="checkbox" id="sdg17" v-model="checkedSDG" value="17"/>
         <label class="sdglabel" for="sdg17">
           <img src="https://www.undp.org/content/dam/undp/sdg/tiles/sdg-en-17.png"    alt="Partnerships for the Goals" />
         </label>
@@ -188,15 +188,32 @@
         <p>Strengthen the means of implementation and revitalize the global partnership for sustainable development.</p>
       </div>
     </div>
+    <div
+      class="d-flex align--center justify--space-between down-container">
+      <button class="btn btn-primary" type="button" v-on:click="sdgSubmit">
+        Done
+      </button>
+    </div>
   </div>
 
 </template>
 
 <script>
+export default {
+  name: 'sdg',
+  data () {
+    return { checkedSDG: [] }
+  },
+  methods: {
+    sdgSubmit: function (event) {
+      console.log(this.checkedSDG)
+    }
+  }
+}
 </script>
 
 <style lang="scss">
-  .sdg-container{
+  #sdg-container{
     display: flex;
     flex-wrap: wrap;
     width: 100%;
