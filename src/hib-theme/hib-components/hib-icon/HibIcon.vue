0447f376415142a5114fd2ff842a18d1a37c68c7
Kunal Hemrajani <kunal.hemrajani@4mation.com.au>
2019-06-23 11:37:10 +1000

    updated title and fixed the height of navbar

--- src/hib-theme/hib-components/hib-icon/HibIcon.vue
+++ src/hib-theme/hib-components/hib-icon/HibIcon.vue
@@ -1,5 +1,8 @@
 <template>
-  <img src="https://static.wixstatic.com/media/ad81e5_0c921866fa3a48baa4d4b9c8666b7b2b~mv2.jpg/v1/fill/w_60,h_60,al_c,q_80,usm_0.66_1.00_0.01/ad81e5_0c921866fa3a48baa4d4b9c8666b7b2b~mv2.webp"/>
+<div>
+  <img src="https://static.wixstatic.com/media/ad81e5_0c921866fa3a48baa4d4b9c8666b7b2b~mv2.jpg/v1/fill/w_40,h_40,al_c,q_80,usm_0.66_1.00_0.01/ad81e5_0c921866fa3a48baa4d4b9c8666b7b2b~mv2.webp"/>
+  <span style="color: black;"> Humanity in Business </span>
+</div>
 </template>
 
 <script>
